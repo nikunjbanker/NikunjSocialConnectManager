86e61ec954afb442cbeb9f2cacb8bd75981214fb
nikunjbanker <nikunj_banker@hotmail.com>
2014-05-08 18:24:08 +0530

    Last for day 01

--- NikunjSocialConnectManager/Controllers/HomeController.cs
+++ NikunjSocialConnectManager/Controllers/HomeController.cs
@@ -1,30 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace NikunjSocialConnectManager.Controllers
+namespace SanketSocialContactManager.Controllers
 {
   public class HomeController : Controller
   {
     [AllowAnonymous]
     public ActionResult Index()
     {
+      Trace.WriteLine("Entering Index method");
+      ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+      Trace.TraceInformation("Displaying the Index page at " + DateTime.Now.ToLongTimeString());
+      Trace.WriteLine("Leaving Index method");
       return View();
     }
 
     public ActionResult About()
     {
-      ViewBag.Message = "Your application description page.";
-
+      Trace.WriteLine("Entering About method");
+      ViewBag.Message = "Your app description page.";
+      Trace.TraceWarning("Transient error on the About page at " + DateTime.Now.ToShortTimeString());
+      Trace.WriteLine("Leaving About method");
       return View();
     }
 
     public ActionResult Contact()
     {
+      Trace.WriteLine("Entering Contact method");
       ViewBag.Message = "Your contact page.";
-
+      Trace.TraceError("Fatal error on the Contact page at " + DateTime.Now.ToLongTimeString());
+      Trace.WriteLine("Leaving Contact method");
       return View();
     }
   }
